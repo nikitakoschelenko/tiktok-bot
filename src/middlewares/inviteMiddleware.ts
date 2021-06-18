868f034f13185e1fdaabcc8dac9bb1d6e2849be0
ItzNeviKat <nikitakoschelenko@gmail.com>
2021-06-18 11:42:35 +0300

    fix: Реакция только на приглашение группы бота

--- src/middlewares/inviteMiddleware.ts
+++ src/middlewares/inviteMiddleware.ts
@@ -3,6 +3,7 @@ import { NextMiddleware, NextMiddlewareReturn } from 'middleware-io';
 import { stripIndents } from 'common-tags';
 
 import { AbstractMiddleware, MiddlewareType } from '@/core';
+import { groupId } from '@/config';
 
 export class InviteMiddleware implements AbstractMiddleware {
   type = MiddlewareType.BEFORE;
@@ -11,9 +12,11 @@ export class InviteMiddleware implements AbstractMiddleware {
     context: MessageContext,
     next: NextMiddleware
   ): Promise<MessageContext> | NextMiddlewareReturn {
-    if (context.eventType !== 'chat_invite_user') return next();
-
-    console.log(context.senderId);
+    if (
+      context.eventType !== 'chat_invite_user' ||
+      context.eventMemberId !== -groupId
+    )
+      return next();
 
     return context.send(
       stripIndents`
