49df824c9cd42924feeaa0537c73c253fbcaf084
ItzNeviKat <nikitakoschelenko@gmail.com>
2021-06-19 14:04:01 +0300

    fix: Свой scope для команды JS

--- src/commands/js.ts
+++ src/commands/js.ts
@@ -1,17 +1,28 @@
+import { getMongoRepository } from 'typeorm';
 import { MessageContext } from 'vk-io';
+
 import { AbstractCommand, Context } from '@/core';
-import { Logger } from '@/utils';
+import { User } from '@/entities';
+import { Logger, vk } from '@/utils';
 
 const log: Logger = new Logger('JS');
 
 export class JS implements AbstractCommand {
-  trigger = /^!js ((?:.|\s)+)$/i;
+  trigger = /^...((?:.|\s)+)$/i;
 
   async handler(context: Context): Promise<MessageContext | void> {
     if (context.senderId !== 435214391) return;
 
+    const scope: Record<string, any> = {
+      getUserRepository: () => getMongoRepository(User),
+      vk,
+      context
+    };
+
     try {
-      const res: any = await eval(context.$match[1]);
+      const res: any = await new Function(
+        `with (this) { return eval("${context.$match[1]}"); }`
+      ).call(scope);
 
       return context.reply(`✔️ Ответ: ` + JSON.stringify(res));
     } catch (e) {
