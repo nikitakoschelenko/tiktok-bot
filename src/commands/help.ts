c137770884f22f4df58071c618c160d2f20f268a
Georgiy Panin <NovaStream2030@ya.ru>
2021-07-10 14:44:29 +0700

    fix: команда помощи в чате

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -3,10 +3,13 @@ import { stripIndents } from 'common-tags';
 import { AbstractCommand, Context } from '@/core';
 
 export class Help implements AbstractCommand {
-  trigger = /^\/помощь|начать|start$/i;
+  trigger = /^\/помощь|^начать$|^start$/i;
   payload = 'start';
 
   async handler(context: Context) {
+  if (context.isChat && !content.text.match(/^\/помощь/i))
+    return ;
+
     return context.send(stripIndents`
       😊 Я умею отвечать на сообщение с ссылкой на TikTok видео по этой ссылке. Предназначен для использования в беседе, но могу и в личных сообщениях :3
       📚 Подробнее обо мне и моей настройке - https://vk.com/@tiktokbot-info
